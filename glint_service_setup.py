6196e3f73248758d1cd49bc6f365eee8d6f3fd5b
rd <rd@rat01.heprc.uvic.ca>
2014-09-15 10:40:51 -0700

    added an uninstall to glint for service removal from openstack db

--- glint_service_setup.py
+++ glint_service_setup.py
@@ -15,7 +15,7 @@
 # limitations under the License.
 
 # THIS FILE IS MANAGED BY THE GLOBAL REQUIREMENTS REPO - DO NOT EDIT
-import os,re
+import os,re,sys
 import subprocess
 
 #ensure environment variables are set
@@ -34,32 +34,60 @@ def environment_check():
 
 # use keystone to register glint as a service
 def setup_glint_service():
-    print "setting up table service entry and endpoint entry into openstack db usng the keystone service"
-    #check if service already registered
-    process = subprocess.Popen(['keystone','service-list'],stdout=subprocess.PIPE,stderr=subprocess.PIPE)      
+    print "Setting Up Glint ... Remove any Old DB Entries"
+    remove_glint_service()
+    print "Registering Glint into Openstack's Keystone services and endpoint database"
+    process = subprocess.Popen(['keystone','service-create','--name=glint','--type=image_mgt','--description="Image Distribution Service"'],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    out,err = process.communicate()
+    rexp_processor = re.compile('\|\s+id\s+\|\s+[a-z0-9]{32}\s+\|')
+    rexp_res = rexp_processor.search(out)
+    rexp_processor = re.compile('[a-z0-9]{32}')
+    glint_service_id = rexp_processor.search(rexp_res.group()).group()
+    process = subprocess.Popen(['keystone','endpoint-create','--region=openstack','--service-id=%s'%glint_service_id,'--publicurl=http://rat01.heprc.uvic.ca:9494/image_dist/','--internalurl=http://127.0.0.1:9494/','--adminurl=http://rat01.heprc.uvic.ca:9494/admin'],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    out,err = process.communicate()
+    print "Success Registering Glint to Openstack's Keystone database"
+    return
+
+def remove_glint_service():
+    print "Remove and old registered glint services"
+    process = subprocess.Popen(['keystone','service-list'],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     out,err = process.communicate()
     if "glint" in out:
-        print "glint has been registered"
-        return
-    else:
-        print "glint has not been registered, so register it."
-        print "Create Service Glint"
-        process = subprocess.Popen(['keystone','service-create','--name=glint','--type=image_mgt','--description="Image Distribution Service"'],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-        out,err = process.communicate()
-        rexp_processor = re.compile('\|\s+id\s+\|\s+[a-z0-9]{32}\s+\|')
+        print "Glint has been registered so lets remove it "
+        rexp_processor = re.compile('\|\s+[a-z0-9]{32}\s+\|\s+glint\s+\|')
         rexp_res = rexp_processor.search(out)
         rexp_processor = re.compile('[a-z0-9]{32}')
         glint_service_id = rexp_processor.search(rexp_res.group()).group()
-        print "Service id %s"%glint_service_id
-        print "Create Glint Endpoint"
-        process = subprocess.Popen(['keystone','endpoint-create','--region=openstack','--service-id=%s'%glint_service_id,'--publicurl=http://rat01.heprc.uvic.ca:8081/image_dist/','--internalurl=http://127.0.0.1:8081/','--adminurl=http://rat01.heprc.uvic.ca:8081/admin'],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        #remove the glint_service_id using keystone using keystone service-delete id
+        process = subprocess.Popen(['keystone','service-delete','%s'%glint_service_id],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
         out,err = process.communicate()
-        return
 
+        #locate the glint endpoint refenence using glint_service_id and delete using endpoint-delete id
+        process = subprocess.Popen(['keystone','endpoint-list'],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        out,err = process.communicate()
+        if glint_service_id in out:
+            #print "keystone ep list %s"%out
+            #regex_str = '\|\s+[a-z0-9]{32}\s+\|\s+\w+\s+\|\s+[a-zA-Z0-9/.:%()]+\s+\|\s+.+\s+\|\s+.+\s+\|\s+%s\s+\|'%glint_service_id
+            regex_str = '\|\s+[a-z0-9]{32}\s+\|\s+\w+\s+\|\s+[a-zA-Z0-9:/._()-]+\s+\|\s+[a-zA-Z0-9:/._()-]+\s+\|\s+[a-zA-Z0-9:/._()-]+\s+\|\s+%s\s+\|'%glint_service_id
+            #print "regex str %s"%regex_str
+            rexp_processor = re.compile(regex_str)
+            glint_endpoint_id = rexp_processor.search(out).group()
+            #print "found endid %s"%glint_endpoint_id
+            process = subprocess.Popen(['keystone','endpoint-delete','%s'%glint_endpoint_id],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+            out,err = process.communicate()
+    else:
+        print "Glint has not been registered yet, so let's register it."
+    return
 env_ck = environment_check()
 
 if env_ck:
-    setup_glint_service()
+    if len(sys.argv) == 2:
+        if sys.argv[1] == 'uninstall':
+            remove_glint_service()
+        else:
+            print "invalid argument ... only valid argument is uninstall"    
+    else:
+        setup_glint_service()
 else:
     print "Unable to setup glint service for use by openstack becuase the environment variables need to be set by the administrator, please source the admins .rc file which you can get from the openstack horizon interface Access and Security section"
 
