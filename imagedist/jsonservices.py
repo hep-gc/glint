71ecfa2414fd0174f557782e1cdeb7ecc30f1c55
ronaldjosephdesmarais <ron.desmarais@gmail.com>
2014-10-16 15:12:15 -0700

    added cred management glint service

--- imagedist/jsonservices.py
+++ imagedist/jsonservices.py
@@ -396,7 +396,7 @@ def hascredential(request):
         site_id = request.POST['SITE_ID']
         user_name = request.POST['USER_ID']
         print "have un: %s and site id :%s "%(user_name,site_id)
-        users = user.objects.filter(username=user_name)
+        users = user.objects.filter(username=user_name,tenent=request.POST['USER_TENANT'])
         user_id = users.pk
         #user_id = user_obj[0].pk
         print "site id hopefully %s and user name %s id is %s"%(site_id,user_name,user_id)
