1c30d2241dc932db665ea2a81ac4dca2f9578c5c
ronaldjosephdesmarais <ron.desmarais@gmail.com>
2014-11-20 13:35:38 -0800

    added arg parser support for glint_backup

--- glint_backup/glintargparse.py
+++ glint_backup/glintargparse.py
@@ -12,17 +12,11 @@ class GlintArgumentParser:
         print "Init GlintArgumentParser"
         self.parser = argparse.ArgumentParser(description='Glint\'s Backup Argument Parser')
    
-    def init_backup_arg_parser(self):
+    def init_restore_arg_parser(self):
         self.parser.add_argument("-version",nargs=1)
         self.parser.add_argument("-cfgfile",nargs=1)
-        #self.parser.add_argument("-install",nargs=1,choices=['all','glint','horizon'])
-        #self.parser.add_argument("-uninstall",nargs=1,choices=['all','glint','horizon'])
-        #self.parser.add_argument("-glint_url")
-        #self.parser.add_argument("-glint_hor_url")
-        #self.parser.add_argument("-glint_inst_type")
-        #self.parser.add_argument("-hor_inst_type")
-        #self.parser.add_argument("-glint_server",nargs=1,choices=['django','paste'])
-        #self.parser.add_argument("-glint_horizon_server",nargs=1,choices=['django','apache'])
-        #self.parser.add_argument("-glint_user_id")
+    
+    def init_backup_arg_parser(self):
+        self.parser.add_argument("-cfgfile",nargs=1)    
 
     
