7fe5d0edd99b434ffeb48a55ebc50ece01635d62
ronaldjosephdesmarais <ron.desmarais@gmail.com>
2014-11-25 14:24:58 -0800

    changing format of arguments

--- glint_backup/glintargparse.py
+++ glint_backup/glintargparse.py
@@ -13,11 +13,11 @@ class GlintArgumentParser:
         self.parser = argparse.ArgumentParser(description='Glint\'s Backup Argument Parser')
    
     def init_restore_arg_parser(self):
-        self.parser.add_argument("-c","--cfgfile",nargs=1)
-        self.parser.add_argument("-v","--version",nargs=1)
-        self.parser.add_argument("-t","--tenant",nargs=1)
-        self.parser.add_argument("-i","--image-name",nargs='*')
-        self.parser.add_argument("-l","--list-images",action='store_true')
+        self.parser.add_argument("-c","--cfgfile",nargs=1,help="Required Configuration File for restore or uses default")
+        self.parser.add_argument("-v","--version",nargs=1,help="This Parameter is required or tool will fail")
+        self.parser.add_argument("-t","--tenant",nargs=1,help="Specify a tenant directory you wish to restore, rather than all tenants of a versioning")
+        self.parser.add_argument("-i","--image-name",nargs='*',help="Used with the --tenant option if you wish to restore a single image from a tenant")
+        self.parser.add_argument("-l","--list-images",action='store_true',hel="Used with --tenant to list images in a tenant at the specifed --version")
         
         
     def init_backup_arg_parser(self):
