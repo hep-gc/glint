bca9ef6dc08ccc58d1d5c4e4a84e2939f1386cd2
ronaldjosephdesmarais <ron.desmarais@gmail.com>
2014-11-25 16:02:32 -0800

    changing format of arguments

--- glint_backup/glint_print_util.py
+++ glint_backup/glint_print_util.py
@@ -9,4 +9,5 @@ def print_line(cols):
     print "+%s+%s+%s+%s+%s+%s"%('-'*cols[0],'-'*cols[1],'-'*cols[2],'-'*cols[3],'-'*cols[4],'-'*cols[5])
     
 def print_line_title(titles):
-    print "|%s|%s|%s|%s|%s|%s|"%(titles[0],titles[1],titles[1],titles[2],titles[3],titles[4],titles[5])
+    print titles
+    print "|%s|%s|%s|%s|%s|%s|"%(titles[0],titles[1],titles[2],titles[3],titles[4],titles[5])
