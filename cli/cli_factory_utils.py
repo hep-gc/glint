7b31dda4a38e98f1c994047dad543329aadeda9d
ronaldjosephdesmarais <ron.desmarais@gmail.com>
2015-03-31 17:31:40 -0700

    added sizing of columns functionality

--- cli/cli_factory_utils.py
+++ cli/cli_factory_utils.py
@@ -6,10 +6,14 @@ Created on Mar 31, 2015
 
 titles = {
           "list-sites":[{"header":"Site Name","key":"name"},{"header":"Site URL","key":"url"},{"header":"Cloud Type","key":"type"}],
+          "basic-result":[{"header":"Result of Operation","key":"Result"}],
+          "basic-result-creds":[{"header":"Result of Operation","key":"result"}],
+          "create-site-result":[{"header":"Result of Operation","key":"Result"},{"header":"site id","key":"site_id"}],
+          "get-credential":[{"header":"Credential id","key":"cred_id"},{"header":"Tenant Name","key":"tenant"}],
+          "image-operation":[{"header":"Image Operation Thread id","key":"thread_id"}],
           }
 
 def get_titles(cmd):
-    #read in cfg file to get titles
     return titles[cmd]
 
 def get_max_column_sizes(json,header):
@@ -22,14 +26,12 @@ def get_max_column_sizes(json,header):
     return sizes        
    
 def _handle_json_object_array(json,key): 
-    max=0
+    _max_size=0
     for obj in json:
-        print obj
-        print "Found %s"%obj["name"]
-        size = len(obj[key])
-        if max < size:
-            max = size
+        size = len("%s"%obj[key])
+        if _max_size < size:
+            _max_size = size
         
-    return max
+    return _max_size
         
         
